5cd964f0d6b3b0de40babcacc4dee0a598dbd87f
Dirk Dobson <dirkvancouver@live.com>
2018-08-31 17:01:39 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+require "faker"
+include Faker
+
+5.times do
+  AnimalDetail.create(
+    name: Faker::Name.name,
+    age: Faker::Number.number(2),
+    hair_color: Faker::Name.hair_color,
+    eye_color: Faker::Name.eye_color,
+    gender: Faker::Internet.gender,
+    alive: Faker::Internet.alive
+  )
+
+end
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
